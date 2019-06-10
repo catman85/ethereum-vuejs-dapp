131b0998e7f38c595791a179726a9cb905d42166
jimmy <test@protonmail.com>
2019-06-11 01:37:48 +0300

    Contract Done?

--- contracts/Degrees.sol
+++ contracts/Degrees.sol
@@ -2,7 +2,10 @@ pragma solidity ^0.5.0;
 
 contract Degrees {
     address private owner;
-    mapping(string => bool) public graduates;
+    uint private k;// the required amount of signatures to be considered a graduate
+
+    // the string will be the hash of the graduates info
+    mapping(string => Professor[]) public graduates;
     // mapping(address => Professor) public professors;
 
     Professor[] public professors;
@@ -17,7 +20,7 @@ contract Degrees {
 
     // _; tells us where the body of the caller function will be injected
     modifier isOwner() {
-        require(msg.sender == owner);
+        require(msg.sender == owner, "You are not the contract's owner");
         _;
     }
 
@@ -25,13 +28,13 @@ contract Degrees {
         bool b = false;
         address s = msg.sender;
 
-        for(uint8 i=0; i<professors.length; i++){
+        for(uint8 i = 0; i<professors.length; i++){
             if(professors[i].profAddress == s){
-                b=true;
+                b = true;
             }
         }
 
-        require(b);
+        require(b,"Not a Professor");
         _;
     }
 
@@ -42,7 +45,7 @@ contract Degrees {
     }
 
     // this function must only be called at the contract's creation
-    function addProf(address _profAddress, string memory _name) isOwner private {
+    function addProf(address _profAddress, string memory _name) private isOwner{
         Professor memory p = Professor({
             profAddress: _profAddress,
             name: _name,
@@ -52,5 +55,28 @@ contract Degrees {
         professors.push(p);
 
         emit newProfessorRegistered(p.id);
+    }
+
+    function verifyGraduation(string memory _hash) public returns(bool){
+        Professor[] memory approvals = graduates[_hash];
+        if(approvals.length >= k){
+            return true;
+        }
+        return false;
+    }
+
+    function signGraduation(string memory hash) public isProf{
+        int index = getProfessorIndex();
+        require(index>=0,"Only Professors can sign graduations");
+        graduates[hash].push(professors[uint(index)]);
+    }
+
+    function getProfessorIndex() public isProf returns(int){
+        for(uint i = 0; i<professors.length; i++){
+            if(professors[i].profAddress == msg.sender){
+                return int(i);
+            }
+        }
+        return -1;
     }
 }
